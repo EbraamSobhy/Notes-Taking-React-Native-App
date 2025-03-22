7b01b26701ae4c4d984c0370caf551e184a8dcf7
Ebraam Sobhy <ibraam.sobhy2003@gmail.com>
2025-03-22 14:43:44 +0200

    first commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Notes from './components/Notes';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Notes" component={props => <Notes {...props} />} />
+        </Stack.Navigator>
+      </NavigationContainer>
+  );
+}
---
+++ components/Notes.jsx
@@ -0,0 +1,161 @@
+import { View, Text, StyleSheet } from 'react-native';
+import { color } from '../assets/styles';
+const Notes = () => {
+    return (
+        <View style={styles.notesContainer}>
+
+            <View style={styles.headingContainer}>
+                <Text style={styles.heading}>Welcome to Notes</Text>
+            </View>
+
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    notesContainer: {
+        paddingTop: 10,
+        paddingHorizontal: 20,
+        marginBottom: 70,
+        opacity: 0.9,
+    },
+    heading: {
+        fontSize: 20,
+        fontWeight: '700',
+        color: color,
+    },
+
+    divider: {
+        width: '100%',
+        height: 2,
+        backgroundColor: color,
+        marginTop: 5,
+        marginBottom: 5,
+    },
+
+    item: {
+        marginBottom: 20,
+        padding: 15,
+        color: 'black',
+        opacity: 0.8,
+        marginTop: 10,
+        shadowColor: color,
+        shadowOpacity: 0.5,
+        shadowOffset: { width: 0, height: 4 },
+        shadowRadius: 8,
+        elevation: 5,
+        backgroundColor: 'white',
+        borderColor: color,
+        borderWidth: 2,
+        borderRadius: 5,
+        borderLeftWidth: 15,
+    },
+
+    index: {
+        fontSize: 20,
+        fontWeight: '800',
+    },
+
+    headingContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+
+    button: {
+        backgroundColor: color,
+        width: 50,
+        borderRadius: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        height: 50,
+    },
+
+    buttonText: {
+        color: 'white',
+        fontSize: 32,
+        fontWeight: '800',
+    },
+
+    scrollView: {
+        marginBottom: 70,
+    },
+
+    note: {
+        flexDirection: 'row',
+        width: '75%',
+    },
+
+    text: {
+        fontWeight: '700',
+        fontSize: 17,
+        alignSelf: 'center',
+    },
+
+    delete: {
+        color: color,
+        fontWeight: '700',
+        fontSize: 15,
+    },
+
+    input: {
+        height: 40,
+        paddingHorizontal: 20,
+        width: '65%',
+        fontSize: 19,
+        color: 'black',
+        fontWeight: '600',
+        opacity: 0.8,
+        shadowColor: color,
+        shadowOpacity: 0.4,
+        shadowOffset: { width: 0, height: 4 },
+        shadowRadius: 8,
+        elevation: 5,
+        backgroundColor: 'white',
+        borderColor: color,
+        borderWidth: 2,
+        borderRadius: 5,
+    },
+
+    searchContainer: {
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginVertical: 8,
+    },
+
+    searchButton: {
+        backgroundColor: color,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 60,
+        borderRadius: 5,
+        height: 40,
+    },
+
+    searchButtonText: {
+        color: 'white',
+        fontWeight: '700',
+        fontSize: 12
+    },
+
+    emptyNoteContainer: {
+        alignItems: 'center',
+        marginTop: 240
+    },
+
+    emptyNoteText: {
+        color: color,
+        fontWeight: '600',
+        fontSize: 15,
+    },
+
+    dateContainer: {
+        marginTop: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+});
+
+export default Notes;
